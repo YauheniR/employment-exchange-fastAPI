295cc9b53b1c0e12dafb5840c4e671d82f341a18
YauheniR <Yauheni.conn@gmail.com>
2021-12-09 14:15:07 +0300

    Feature - create repositories methods and hash, verify password

--- repositories/users.py
+++ repositories/users.py
@@ -1,5 +1,9 @@
+import datetime
 from typing import List
+from typing import Optional
 
+from core.security import hashed_password
+from db import users
 from models.user import User
 from models.user import UserIn
 from repositories.base import BaseRepository
@@ -7,16 +11,55 @@ from repositories.base import BaseRepository
 
 class UserRepository(BaseRepository):
     async def get_all(self, limit: int = 100, skip: int = 0) -> List[User]:
-        return
+        query = users.select().limit(limit).offset(skip)
+        return await self.database.fetch_all(query=query)
 
-    async def get_by_id(self, id: int) -> User:
-        return
+    async def get_by_id(self, id: int) -> Optional[User]:
+        query = users.select().where(users.c.id == id).first()
+        user = await self.database.fetch_one(query=query)
+        if user is None:
+            return None
+        return User.parse_obj(user)
 
     async def create(self, u: UserIn) -> User:
-        return
+        user = User(
+            name=u.name,
+            hashed_password=hashed_password(u.password),
+            email=u.email,
+            is_company=u.is_company,
+            created_at=datetime.datetime.utcnow(),
+            updatet_at=datetime.datetime.utcnow(),
+        )
 
-    async def update(self, u: UserIn) -> User:
-        return
+        values = {**user.dict()}
+        values.pop("id", None)
 
-    async def get_by_email(self, email: str) -> User:
-        return
+        query = users.insert().values(**values)
+        user.id = await self.database.execute(query=query)
+        return user
+
+    async def update(self, id: int, u: UserIn) -> User:
+        user = User(
+            id=id,
+            name=u.name,
+            hashed_password=hashed_password(u.password),
+            email=u.email,
+            is_company=u.is_company,
+            created_at=datetime.datetime.utcnow(),
+            updatet_at=datetime.datetime.utcnow(),
+        )
+
+        values = {**user.dict()}
+        values.pop("id", None)
+        values.pop("created_at", None)
+
+        query = users.update().where(users.c.id == id).values(**values)
+        await self.database.execute(query=query)
+        return user
+
+    async def get_by_email(self, email: str) -> Optional[User]:
+        query = users.select().where(users.c.email == email).first()
+        user = self.database.fetch_one(query=query)
+        if user is None:
+            return None
+        return User.parse_obj(user)
